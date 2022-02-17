3f6786ef37c22962325b3614412ab9afea28f51e
kimlah <71352287+kimlah@users.noreply.github.com>
2022-02-16 21:35:20 -0800

    week 7

--- Week7/rfk-incomplete/rfk/game/casting/artifact.py
+++ Week7/rfk-incomplete/rfk/game/casting/artifact.py
@@ -1,3 +1,29 @@
 from game.casting.actor import Actor
 
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    """A collection of Artifacts
+
+    The responsibility of Artifact is to display a message when touched
+    
+    Attributes:
+    _message
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._message=""
+
+    def get_message(self):
+        """Get message for artifact
+           Returns: 
+           message string
+        """
+        return self._message
+
+    def set_message(self, message):
+        """updates message to chosen one
+           Args:
+           message: selected message string
+        """
+        self._message = message
